13a5afa41ec87b2879c162c22c71840e6975a66e
Jean Piere <ljpcastroc@gmail.com>
2025-03-25 20:18:55 -0500

    Sprint 6 class

--- DA/Webinar 18 - Sprint 6 - M1M2/01_getpython.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-URL = 'https://tripleten-com.github.io/simple-shop_es/'
-req = requests.get(URL)
-
-# quizas esto no imprime nada?
--- DA/Webinar 18 - Sprint 6 - M1M2/02_getpython.py
+++
@@ -1,8 +0,0 @@
-import requests
-import re
-
-URL = 'https://tripleten-com.github.io/simple-shop_es/'
-req_text = requests.get(URL).text
-found_products = re.findall('Horizon[ \w\-%]+', req_text)
-print(len(found_products))
-print(found_products)
