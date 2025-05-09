fb5b67741e844b4dfb65e234ca25547db6787b87
Jean Piere <ljpcastroc@gmail.com>
2025-05-09 14:19:27 -0500

    antoher example

--- python_s2_ex.py
+++ python_s2_ex.py
@@ -4,3 +4,11 @@ for i in lista_numeros:
     conteo -= i
 
 print(conteo)
+
+#---------------------------------------------------------------------
+# Suponiendo que el dataframe que queremos
+# revisar se llama ords
+
+for i in order_products.columns:
+    calculo = order_products[i].isna().sum()
+    print("Nombre columna: ", i, ". Cantidad de vacíos:", calculo)
