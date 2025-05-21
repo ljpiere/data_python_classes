447e37b3940bd170b4881c868a9bf5590d9ca34e
Jean Piere <ljpcastroc@gmail.com>
2025-05-20 23:54:08 -0500

    test folder

---
+++ test/test.py
@@ -0,0 +1,9 @@
+# tests/test_arithmetic_green.py
+
+def test_addition():
+    # Ahora pasará: 1 + 1 == 2
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    # Ahora pasará: 5 - 2 == 3
+    assert 5 - 2 == 3
